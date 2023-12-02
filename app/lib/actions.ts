fdd25837374af0e5bcb5273a942a5de04258cbff
Flosrn <flosrn.dev@gmail.com>
2023-12-02 17:15:21 +0100

    Implement user authentication in LoginForm
    Integrated user authentication in LoginForm component by adding `authenticate` function from `actions.ts`. Added the handling of form errors and disabled button functionality during pending status in LoginForm. This allows for signIn using the Credentials provider and handles any authentication errors.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -116,5 +117,24 @@ export async function deleteInvoice(prevState: State, id: string) {
     return {
       message: "Database Error: Failed to Delete Invoice.",
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
